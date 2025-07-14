dd1fc43d3190ab9e787c1cbd94f53f386859cc88
Developershakya <shakyadeveloper@gmail.com>
2025-07-14 14:12:13 +0530

    dynamic director and supervisor

--- utils/reminderScheduler.js
+++ utils/reminderScheduler.js
@@ -237,6 +237,6 @@ const runEscalationJob = async () => {
 };
 
 // ⏱️ Cron Job (every 2 mins for testing)
-cron.schedule('*/2 * * * *', () => {
+cron.schedule('*/60 * * * *', () => {
   runEscalationJob();
 });
